b658ac4f11b63be32d045043b15ec5f97d7eacf5
zhouh <23862691@qq.com>
2018-03-16 12:29:48 +0800

    first commit

---
+++ zPluto-common/src/main/java/cn/zPluto/App.java
@@ -0,0 +1,13 @@
+package cn.zPluto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

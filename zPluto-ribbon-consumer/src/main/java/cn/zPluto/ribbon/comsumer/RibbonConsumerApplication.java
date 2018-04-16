b951ef58437045eb0aff8ede26d41ee1687ad66e
zhouh <23862691@qq.com>
2018-04-16 18:33:41 +0800

    fix

--- zPluto-ribbon-consumer/src/main/java/cn/zPluto/ribbon/comsumer/RibbonConsumerApplication.java
+++ zPluto-ribbon-consumer/src/main/java/cn/zPluto/ribbon/comsumer/RibbonConsumerApplication.java
@@ -1,9 +1,12 @@
 package cn.zPluto.ribbon.comsumer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,8 +18,11 @@ import org.springframework.web.client.RestTemplate;
  * @Date 2018/4/12.
  */
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableAutoConfiguration
 public class RibbonConsumerApplication {
 
 

7516087429f5c6622d1d34c370070233773e1af9
zhouh <23862691@qq.com>
2018-04-24 19:39:31 +0800

    fix

--- zPuto-api-zuul/src/main/java/cn/zPluto/api/zuul/ApiZuulApplication.java
+++ zPuto-api-zuul/src/main/java/cn/zPluto/api/zuul/ApiZuulApplication.java
@@ -10,7 +10,9 @@ import org.springframework.context.annotation.Bean;
 
 /**
  * <p>Description:</p>
- *
+ * spring-cloud-starter-zuul,自身包含了对spring-cloud-starter-hystrix和spring-cloud-starter-ribbon,
+ * zuul 天生就拥有线程隔离和断路器的自我保护功能，以及对服务调用的客户端负载均衡功能.
+ * 我们zuul通常是用作请求转发以及请求过滤两个功能
  * @author zhouh
  * @version 1.0
  * @Date 2018/4/17.
